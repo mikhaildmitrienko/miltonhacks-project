e46c33b3dfcfdd7d9bade23da77d6ce9c3cd07cb
Sebastian Park <SebPark03@gmail.com>
2020-05-24 09:53:15 -0400

    Add Firebase Config.

---
+++ webapp/public/js/firebaseCfg.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyA_jaJFyahK5tDzWsUVMUuWFwmCR6YUelE",
+    authDomain: "miltonhacksii.firebaseapp.com",
+    databaseURL: "https://miltonhacksii.firebaseio.com",
+    projectId: "miltonhacksii",
+    storageBucket: "miltonhacksii.appspot.com",
+    messagingSenderId: "561093652964",
+    appId: "1:561093652964:web:eadcebdd97e1da7b64d7f1",
+    measurementId: "G-10Z82ZNDH2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
