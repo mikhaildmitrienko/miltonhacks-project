9bb09f593ce9d79d1dffed67651f0dbeb0166a94
mikhaildmitrienko <michael.dmitrienko@gmail.com>
2020-05-24 15:55:39 -0400

    added yelp and distance functions

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const admin = require('firebase-admin');
 const bodyParser = require('body-parser');
-const yelp_api = require('../server/utils/yelp');
+const request = require('request');
+//const yelp_api = require('../server/utils/yelp');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -12,20 +13,42 @@ const yelp_api = require('../server/utils/yelp');
 //  response.send("Hello from Firebase!");
 // });
 
-const app = express();
+// const app = express();
 
-const jsonParser = bodyParser.json();
-app.use(cors({ origin: true }));
+// const jsonParser = bodyParser.json();
+// app.use(cors({ origin: true }));
 
-app.post('/restuarant_finder', jsonParser, (req, res) => {
+// app.post('/restuarant_finder', jsonParser, (req, res) => {
 
-    yelp_api.search(req.body.term, req.body.location, req.body.sortBy).then(jsonResponse =>{
-        res.status(201).send(jsonResponse);
+//     yelp_api.search(req.body.term, req.body.location, req.body.sortBy).then(jsonResponse =>{
+//         res.status(201).send(jsonResponse);
+//     });
+// });
+
+// exports.widgets = functions.https.onRequest(app);
+apiKey = 'pQ18CTIwUG2SkogWiPpuAXA57XiRnDjLNMrBOoEug3IDPv8kEkiwrz_WnWmzLEc0ed2yWgwwgTCXN7VCeW9dm6MX8kLCW4tVr--x6yWASOwWrgDkjvuf_fc5eseoXnYx';
+
+exports.yelp = functions.https.onRequest((req, res) => {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?latitude=${req.latitude}&longitude=${req.longitude}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      res.status(201).send(response);
     });
+  }).catch(err =>{
+      return res.status(404).send(err);
+  });
+
+exports.distance = functions.https.onRequest((req, res)=>{
+    return fetch(`https://maps.googleapis.com/maps/api/directions/json?
+    origin=${req.body.user.latitude,req.body.user.longitude}&
+    destination=${req.body.restuarant.latitude,req.body.restuarant.longitude}
+    &key=AIzaSyA_jaJFyahK5tDzWsUVMUuWFwmCR6YUelE`).then(response => 
+        {return response.json()}).then(jsonResponse =>{
+            res.status(201).send(JSON.stringify(jsonResponse.routes.legs.distance));
+        })
 });
-
-
-exports.widgets = functions.https.onRequest(app);
 
 
 exports.accountCreate = functions.auth.user().onCreate(user => {
