c61c3257261a8dad514d6c2e8fcce82e7acb7d8d
Sebastian Park <SebPark03@gmail.com>
2020-05-24 13:14:18 -0400

    Add navbar animation.

--- webapp/public/js/animation.js
+++ webapp/public/js/animation.js
@@ -6,11 +6,18 @@ window.onload = function () {
 
 function init() {
     document.body.classList.remove('fade-out');
+    var navTop = document.getElementById("navigation-top").style;
     var topBG = document.getElementById("top-bg").style;
     window.onscroll = function () { scrollFunction(); };
     function scrollFunction() {
         console.log(document.documentElement.scrollTop)
         topBG.backgroundPositionY = -(document.documentElement.scrollTop/10) + "px";;
+        if(document.documentElement.scrollTop>=450){
+            navTop.backgroundColor = "#e9e9e9";
+        }
+        else{
+            navTop.backgroundColor = "transparent";
+        }
     }
 
 }
